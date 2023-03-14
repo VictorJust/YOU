9fae1999f68694c1cf9107a4f11c3f0411594b46
VictorJust <victorjust09@gmail.com>
2023-03-14 12:28:43 +0300

    Add assets, playerController & spawner scripts
    Add:
    1. Assets for background, asteroids and music;
    2. playerController script for players' movements;
    3. spawnManagerscript for spawning asteroids from the right side of the
       screen.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private GameObject player;
+    // Set player's movement
+    [SerializeField] private float speed = 10;
+    private float verticlaInput;
+    private float horizontalInput;
+    
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+        verticlaInput = Input.GetAxis("Vertical");
+        horizontalInput = Input.GetAxis("Horizontal");
+        Vector3 movement = new Vector3(horizontalInput, verticlaInput, 0);
+        
+        player.transform.Translate(movement * speed * Time.deltaTime);
+        
+        // Keep player within the screen
+        Vector3 position = Camera.main.WorldToViewportPoint (transform.position);
+        position.x = Mathf.Clamp01(position.x);
+        position.y = Mathf.Clamp01(position.y);
+        transform.position = Camera.main.ViewportToWorldPoint(position);
+    }
+}
---
+++ Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    [SerializeField] private List<GameObject> obstacles;
+    [SerializeField] private int spawnDelay = 3;
+    private float xPosition;
+    private float yPosition;
+    private float yRange = 5;
+    private int numberOfObstacles;
+    private int obstacleIndex;
+    private Vector3 spawnPosition;
+
+    private void Start()
+    {
+        StartCoroutine(SpawnPrefabs());
+    }
+
+    IEnumerator SpawnPrefabs()
+    {
+        while (true)
+        {
+            numberOfObstacles = Random.Range(3, 5);
+
+            for (int i = 0; i <= numberOfObstacles; i++)
+            {
+                xPosition = 12;
+                yPosition = Random.Range(-yRange, yRange);
+                obstacleIndex = Random.Range(0, obstacles.Count);
+                spawnPosition = new Vector3(xPosition, yPosition, 0);
+
+                Instantiate(obstacles[obstacleIndex], spawnPosition, Quaternion.identity);
+            }
+            
+            yield return new WaitForSeconds(spawnDelay);
+        }
+    }
+}
